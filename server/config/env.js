6a6127783ea6a5cec6013e840eda283b38235765
yuelight <vaeqfe@gmail.com>
2018-06-25 17:05:10 +0800

    Update env.js

--- server/config/env.js
+++ server/config/env.js
@@ -1,20 +1,7 @@
-let db = 'mongodb://localhost/test';
-let port = 3000;
-
 const host = process.env.HOST || 'localhost';
 const env = process.env.NODE_ENV || 'development';
 
-if (env !== 'development') {
-	db = 'mongodb://localhost/prod';
-	port = 4000;
-}
+export const db = env === 'development' ? 'mongodb://localhost/test' : 'mongodb://localhost/prod';
+export const port = env === 'development' ? 8081 : 4000;
 
-const base = `http://${host}:${port}`;
-
-export default {
-	env,
-	host,
-	port,
-	db,
-	base
-}
+export const base = `http://${host}:${port}`;
